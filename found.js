1f3f3052d15331aaca848372c0fc5cba8aacd543
cajun <12363371+CajunAvenger@users.noreply.github.com>
2024-02-23 21:57:38 -0600

    bugfix, provisional Sloth translation

--- found.js
+++ found.js
@@ -4,18 +4,21 @@ var fs = require('fs');
 for(let l in langObj.languages) {
 	try {
 		let missing = require(`./missing/${l}.json`);
+		let save = false;
 		for(let b in missing) {
 			let into = langObj[b];
 			if(!langObj[b])
 				into = langObj.subTypes[b];
 			for(let t in missing[b]) {
 				if(missing[b][t]) {
-					into[l] = missing[b][t];
+					into[t][l] = missing[b][t];
 					delete missing[b][t];
+					save = true;
 				}
 			}
 		}
-		fs.writeFile(`./missing/${l}.json`, JSON.stringify(missing), () => {});
+		if(save)
+			fs.writeFile(`./missing/${l}.json`, JSON.stringify(missing, null, 1), () => {});
 	}catch(e) {
 		console.log(e);
 		console.log(`Couldn't open missing json for ${l}.`)
